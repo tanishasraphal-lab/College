cf55068cf773c817ce4d8b1c3b75a59f72e752d7
tanishasraphal <tanishasraphal@gmail.com>
2026-06-08 11:02:28 +0530

    Added html files for web tech and modified Perm.java for stack permutation problem.

--- STS4006/Stack Permutation/Perm.java
+++ STS4006/Stack Permutation/Perm.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public class Perm {
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter number of elements: ");
